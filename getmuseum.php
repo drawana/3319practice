a65f9eedfecc16d7094dd764fb5f474a01ecff89
Daniel Rawana <drawana2@uwo.ca>
2018-12-03 18:08:40 -0500

    display musnames

--- getmuseum.php
+++ getmuseum.php
@@ -5,7 +5,7 @@ if (!$result) {
     die("databases query failed.");
 }
 while ($row = mysqli_fetch_assoc($result)) {
-    echo '<option>$row["musname"]</option>';
+    echo "<option>" .$row["musname"]. "</option>";
 }
 mysqli_free_result($result);
 ?>
